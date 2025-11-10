f60d393e54b4d9db0604d74ac2d5eab3f99abf36
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-10 12:45:12 +0100

    Create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ export default function Header() {
 
     return (
         <header>
-
+            <h1 className="text-center bg-light p-5">
+                ARTICLES
+            </h1>
         </header>
     )
 }
