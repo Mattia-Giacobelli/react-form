5a573ddd8a47bebdcb9d3862ff0870f48c51f5ed
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-10 12:57:56 +0100

    CReate an array of objs for articles and render in page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,28 @@
-export default function Main() {
+export default function Main({ array }) {
 
 
     return (
         <main>
+            <div className="container">
+                <div className="row justify-content-center pt-4">
+                    <div className="col-6">
+                        <div class="card">
+                            <ul class="list-group list-group-flush">
+                                {array.map(article => {
+
+                                    return (
+
+                                        <li key={article.id} class="list-group-item">
+                                            {article.title}
+                                        </li>
+                                    )
+                                })}
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </main>
     )
